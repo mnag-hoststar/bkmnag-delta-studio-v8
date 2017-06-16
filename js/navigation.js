1604b76683c91dcf8628b970b9da1cfc30747625
Eva Jutzeler <eju@hoststar.ch>
2017-06-16 16:39:37 +0200

    Styling the the header and navigation. Adapting the twig files to not have a logo and adding other adaptations relevant to the header and navigation.

---
+++ js/navigation.js
@@ -0,0 +1,26 @@
+// ----------------------------------------
+// Delta Studio: JS > Navigation
+// ----------------------------------------
+	
+// Change navigation if it matches tablet size.
+
+var tablet = '(max-width: 768px)';
+var mq = window.matchMedia(tablet);
+
+if (mq.matches) {
+	// Toggles class open to display/hide the whole navigation.
+
+	$('.extendednavigation__navigation-toggle').click(function() {
+		$('#page-zones__template-widgets__extendednavigation-extendednavigation').toggleClass('open');
+	});
+
+	// Toggles class open to display/hide folders.
+
+	$('.navigation-item.folder').each(function() {
+		$(this).click(function() {
+			$(this).parent('.navigation-item.folder').toggleClass('open');
+		});
+	});
+} else {
+  // Toggle Menu becomes a normal menu.
+}
